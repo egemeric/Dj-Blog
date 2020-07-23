b6a6bb2ff45543fc65daffc3932320ec80aa0d87
Ege Meriç <egemeric99@gmail.com>
2020-07-23 16:31:47 +0300

    First Commit

--- python/Blog/Blog/settings.py
+++ python/Blog/Blog/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dj',
         'USER': 'admin',
-        'PASSWORD': 'emerdogan',
+        'PASSWORD': '',
         'HOST' : '127.0.0.1',
         'PORT' : '',
     }
